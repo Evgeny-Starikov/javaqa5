691e4d2f44f182141b8b2ce6c9c26ae2dd280111
Evgeny Starikov <genestar@yandex.ru>
2024-05-05 13:09:14 +0300

    files moddified

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,8 +2,6 @@ public class BmiService {
 
     public int calculate(int weight, double height) {
 
-        //double height = 1.87;
-
         double bmi = weight / Math.pow(height, 2);
 
         return (int) bmi;
